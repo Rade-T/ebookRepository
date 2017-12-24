719f5500e73409042503aeb95b793cc1e552054f
Rade Tontic <rtontic@gmail.com>
2017-12-24 12:37:57 +0100

    Add DTO objects

---
+++ src/main/java/ebookRepository/dto/EbookDTO.java
@@ -0,0 +1,119 @@
+package ebookRepository.dto;
+
+public class EbookDTO {
+
+	private long id;
+
+	private String title;
+
+	private String author;
+
+	private String keywords;
+
+	private int publicationYear;
+
+	private String filename;
+
+	private String MIME;
+
+	private long languageId;
+
+	private long categoryId;
+
+	private long cataloguerId;
+
+	public EbookDTO(long id, String title, String author, String keywords, int publicationYear, String filename,
+			String mIME, long languageId, long categoryId, long cataloguerId) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.keywords = keywords;
+		this.publicationYear = publicationYear;
+		this.filename = filename;
+		MIME = mIME;
+		this.languageId = languageId;
+		this.categoryId = categoryId;
+		this.cataloguerId = cataloguerId;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String getKeywords() {
+		return keywords;
+	}
+
+	public void setKeywords(String keywords) {
+		this.keywords = keywords;
+	}
+
+	public int getPublicationYear() {
+		return publicationYear;
+	}
+
+	public void setPublicationYear(int publicationYear) {
+		this.publicationYear = publicationYear;
+	}
+
+	public String getFilename() {
+		return filename;
+	}
+
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+
+	public String getMIME() {
+		return MIME;
+	}
+
+	public void setMIME(String mIME) {
+		MIME = mIME;
+	}
+
+	public long getLanguageId() {
+		return languageId;
+	}
+
+	public void setLanguageId(long languageId) {
+		this.languageId = languageId;
+	}
+
+	public long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(long categoryId) {
+		this.categoryId = categoryId;
+	}
+
+	public long getCataloguerId() {
+		return cataloguerId;
+	}
+
+	public void setCataloguerId(long cataloguerId) {
+		this.cataloguerId = cataloguerId;
+	}
+}
---
+++ src/main/java/ebookRepository/dto/UserDTO.java
@@ -0,0 +1,86 @@
+package ebookRepository.dto;
+
+public class UserDTO {
+	
+	private long id;
+
+	private String firstname;
+
+	private String lastname;
+
+	private String username;
+
+	private String password;
+
+	private String type;
+
+	private long categoryId;
+
+	public UserDTO(long id, String firstname, String lastname, String username, String password, String type,
+			long categoryId) {
+		super();
+		this.id = id;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.username = username;
+		this.password = password;
+		this.type = type;
+		this.categoryId = categoryId;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public long getCategoryId() {
+		return categoryId;
+	}
+
+	public void setCategoryId(long categoryId) {
+		this.categoryId = categoryId;
+	}
+}
